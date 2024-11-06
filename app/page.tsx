c2c77fda38e3ef832c4d87403444008167356247
Magaiver <magaivermagalhaes.mm@gmail.com>
2024-11-06 17:51:25 -0300

    feat: add authentication with clerk

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,15 @@
-const Home = () => {
+import { UserButton } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+
+const Home = async () => {
+  const { userId } = await auth();
+  if (!userId) {
+    return redirect("/login");
+  }
   return (
-    <div className="flex w-screen items-center justify-center">
-      <p className="text-red-400">Hello World!! </p>
+    <div className="flex h-full items-center justify-center">
+      <UserButton showName />
     </div>
   );
 };
